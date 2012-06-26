9b1f49d09ce206160c3ca9d598829c0632cdfb32
Brendan Whelan <freewheeling.github@hypermatix.com>
2012-06-26 21:24:12 +0100

    assembly name change

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Security")]
+[assembly: AssemblyTitle("Hypermatix.Foundation.Security")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Security")]
+[assembly: AssemblyProduct("Hypermatix.Foundation.Security")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
